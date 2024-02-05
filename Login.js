3c49c1bb014219229519d08ca35ff25193b08897
Lucifer <vivekrai3944@gmail.com>
2024-02-05 09:36:22 +0530

    update

--- Login.js
+++ Login.js
@@ -3,7 +3,7 @@ function login() {
     var password = document.getElementById("password").value;
 
     // Basic validation, you should implement proper authentication logic here
-    if (username === "uuser@gmail.com" && password === "lol") {
+    if (username === "user@gmail.com" && password === "lol") {
       alert("Login successful!");
     } else {
       alert("Invalid username or password. Please try again.");
