1a83057a90a962b9bc306bdd8f7ef72b27bb2e58
Lucifer <vivekrai3944@gmail.com>
2024-02-05 09:33:08 +0530

    update

---
+++ Login.js
@@ -0,0 +1,11 @@
+function login() {
+    var username = document.getElementById("user").value;
+    var password = document.getElementById("password").value;
+
+    // Basic validation, you should implement proper authentication logic here
+    if (username === "uuser@gmail.com" && password === "lol") {
+      alert("Login successful!");
+    } else {
+      alert("Invalid username or password. Please try again.");
+    }
+  }
