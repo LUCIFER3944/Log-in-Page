bfed4f83d0b109c2531cb38a2a7a3d73f2cb048a
Lucifer <vivekrai3944@gmail.com>
2024-02-05 09:40:16 +0530

    update

--- Login.js
+++ Login.js
@@ -2,16 +2,13 @@ document.addEventListener('DOMContentLoaded', function () {
   const loginForm = document.querySelector('form');
 
   loginForm.addEventListener('submit', function (event) {
-    event.preventDefault(); // Prevent the default form submission
+    event.preventDefault();
 
-    // Get user input
     const userEmail = document.getElementById('user').value;
     const userPassword = document.getElementById('password').value;
 
-    // Perform basic validation (you should implement proper validation and authentication logic)
     if (userEmail && userPassword) {
-      alert(`Login successful!\nEmail: ${userEmail}\nPassword: ${userPassword}`);
-      // Here you can redirect or perform other actions upon successful login
+      alert('Login successful!');
     } else {
       alert('Invalid email or password. Please try again.');
     }
